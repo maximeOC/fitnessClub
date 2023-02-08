c4f1f3a2203931b1d7b299a7cdd60a192036852b
maximeOC <maxime1513@gmail.com>
2023-02-08 22:13:51 +0100

    add price in the section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Hero from './components/Hero/Hero';
 import Programms from './components/Programs/Programms';
 import Reasons from './components/Reasons/Reasons';
+import Plans from './components/Plans/Plans';
 function App() {
   return (
     <div className="App">
           <Hero />
           <Programms />
           <Reasons />
+          <Plans />
     </div>
   );
 }
